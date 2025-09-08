87ca4a8c6d094bd2897b17ab574555a8d901a591
Raphael Lamptey <raphlamptey@gmail.com>
2025-09-08 10:51:43 +0000

    the ci/cd branch has been altered

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: true,   // allow access from Windows browser
+        port: 6274,   // 👈 change this
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
